1bc405e3fdaf74ad34e488f1b40c5ed85ddd44fe
sravanI2207 <sravani.avilikonda@gmail.com>
2020-03-07 11:55:41 +0530

    first commit

---
+++ Microservices/loginservice/loginservice/src/main/java/com/project/loginservice/LoginserviceApplication.java
@@ -0,0 +1,16 @@
+package com.project.loginservice;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class LoginserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginserviceApplication.class, args);
+	}
+
+}
---
+++ Microservices/loginservice/loginservice/src/main/java/com/project/loginservice/service/BuyerSignupService.java
@@ -0,0 +1,9 @@
+package com.project.loginservice.service;
+
+import com.project.loginservice.pojo.BuyerSignupPojo;
+
+public interface BuyerSignupService {
+
+	BuyerSignupPojo validateBuyersignup(BuyerSignupPojo buyerSignupPojo);
+	BuyerSignupPojo getBuyer(Integer id);
+}
